2ae4e4ac14c5992304cd98c26e96324530781c49
Victoria Martinez <vmartinez72@live.com>
2019-03-17 17:11:12 -0700

    add gender and planet option to html file

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import './styles.css';
 import {GalaxyAge} from "./scripts.js";
 
 
-// $(document).ready(function(){
-//   $("form#input-age").submit(function(event) {
-//       event.preventDefault();
-//       const inputAge =  $("id#age").val();
-//       const userGalaxyAge = new GalaxyAge();
-//       $("#result").text(userGalaxyAge);
-//     });
-//   });
+$(document).ready(function(){
+  $("form#input-age").submit(function(event) {
+      event.preventDefault();
+      const inputAge =  $("id#age").val();
+      const userGalaxyAge = new GalaxyAge();
+      $("#result").text(userGalaxyAge);
+    });
+  });
