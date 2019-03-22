5c3249bef480b5e1f13f1c49c0150920bd6826b7
Student <example@example.com>
2019-03-21 18:04:33 -0700

    create test to fail and pass

--- spec/spec.js
+++ spec/spec.js
@@ -10,31 +10,25 @@ describe ('GalaxyAge', function() {
   it('should return your age on Earth', function() {
     expect(userAge.age).toEqual(19);
   });
-//
+
   it('should test your age and life exceptancy in Mercury Years',
   function() {
-    const checkMeruryYears = new GalaxyAge(19, "female")
-
-
-    expect(checkMeruryYears.mercuryAge).toEqual(25, "female")
+    expect(userAge.checkMercuryYears(userAge.age, "female")).toEqual("You are 79 in Mercury. And your have 421 more mercury years to live.")
 });
-//
-//   it('should test your age and life expectancy in Venus Years',
-//   function() {
-//     var checkVenusYears = new GalaxyAge(19,"female")
-//     expect(checkVenusYear.venusAge()).toEqual(19,"female")
-//   });
-//
-//   it('should test your age and life expectancy in Mars Year',
-//   function() {
-//     var checkMarsYears = new GalaxyAge(19,"female" )
-//     expect(checkMarsYear.marsYear()).toEqual(19,"female" )
-//   });
-//
-//   it('should test your age in Jupiter Year',
-//   function() {
-//     var checkJupiterYears = new GalaxyAge(19,"female")
-//     expect(checkJupiterYear.jupiterYear()).toEqual(19,"female")
-//   });
+
+  it('should test your age and life expectancy in Venus Years',
+  function() {
+    expect(userAge.checkVenusYears(userAge.age, "female")).toEqual("You are 31 in Venus. And your have 162.5483870967742 more Venus years to live.")
+  });
+
+  it('should test your age and life expectancy in Mars Year',
+  function() {
+    expect(userAge.checkMarsYears(userAge.age, "female")).toEqual("You are 10 in Mars. And your have 53.829787234042556 more Mars years to live.")
+  });
+
+  it('should test your age in Jupiter Year',
+  function() {
+    expect(userAge.checkJupiterYears(userAge.age, "female")).toEqual("You are 2 in Jupiter. And your have 8.118043844856661 more Jupiter years to live.")
+  });
 
 });
--- src/scripts.js
+++ src/scripts.js
@@ -11,15 +11,15 @@ export class GalaxyAge {
   checkMercuryYears(age, gender){
     const mercF = 120 / .24;
     const mercM = 100 / .24;
-    const mercuryAge = this.age / .24;
+    const mercuryAge = Math.round(this.age / .24);
     if (gender === "female"){
       const femaleLifeExpectancy = mercF - mercuryAge;
-      const merFemaleOutput = ("You are " + mercuryAge + ("in Mercury. And your have "  + femaleLifeExpectancy + "more mercury years to live."));
+      const merFemaleOutput = (`You are ${mercuryAge} in Mercury. And your have ${femaleLifeExpectancy} more mercury years to live.`);
       return merFemaleOutput;
     }
     else if (gender === "male") {
       const maleLifeExpectancy = mercM - mercuryAge;
-      const merMaleOutput = ("You are " + mercuryAge + "in Mercury. And your have " + maleLifeExpectancy + "more mercury years to live.");
+      const merMaleOutput = (`You are ${mercuryAge} in Mercury. And your have ${maleLifeExpectancy} more mercury years to live.`);
       return merMaleOutput;
     }
   }
@@ -27,15 +27,15 @@ export class GalaxyAge {
   checkVenusYears(age,gender){
     const venusF = 120 / .62;
     const venusM = 100 / .62;
-    const venusAge =  this.age / .62;
+    const venusAge =  Math.round(this.age / .62);
     if (gender === "female"){
       const femaleLifeExpectancy = venusF - venusAge;
-      const venusFemaleOutput = ("You are " + venusAge + "in Venus. And your have " + femaleLifeExpectancy + "more venus years to live.");
+      const venusFemaleOutput = (`You are ${venusAge} in Venus. And your have ${femaleLifeExpectancy} more Venus years to live.`);
       return venusFemaleOutput;
     }
     else if (gender === "male") {
       const maleLifeExpectancy = venusM - venusAge;
-      const venusMaleOutput = ("You are " + venusAge + "in Venus. And your have " + maleLifeExpectancy + "more venus years to live.");
+      const venusMaleOutput = (`You are ${venusAge} in Venus. And your have ${maleLifeExpectancy} more Venus years to live.`);
       return venusMaleOutput;
     }
   }
@@ -43,15 +43,15 @@ export class GalaxyAge {
   checkMarsYears(age,gender){
     const marsF = 120 / 1.88;
     const marsM = 100 / 1.88;
-    const marsAge = this.age / 1.88;
+    const marsAge = Math.round(this.age / 1.88);
     if (gender === "female") {
       const femaleLifeExpectancy = marsF - marsAge;
-      const marsFemaleOutput = ("You are " + marsAge + "in Mars. And your have " + femaleLifeExpectancy + "more mars years to live.");
+      const marsFemaleOutput = (`You are ${marsAge} in Mars. And your have ${femaleLifeExpectancy} more Mars years to live.`);
       return marsFemaleOutput;
     }
     else if (gender === "male") {
       const maleLifeExpectancy = marsM - marsAge;
-      const marsMaleOutput = ("You are " + marsAge + "in Mars. And your have " + maleLifeExpectancy + "more mars years to live.");
+      const marsMaleOutput = (`You are ${marsAge} in Mars. And your have ${maleLifeExpectancy} more Mars years to live.`);
       return marsMaleOutput;
     }
   }
@@ -59,10 +59,10 @@ export class GalaxyAge {
   checkJupiterYears(age,gender){
     const jupiterF = 120 / 11.86;
     const jupiterM = 100 / 11.86;
-    const jupiterAge =  this.age / 11.86;
+    const jupiterAge =  Math.round(this.age / 11.86);
     if (gender === "female") {
       const femaleLifeExpectancy = jupiterF - jupiterAge;
-      const jupiterFemaleOutput = ("You are " + jupiterAge + "in Jupiter. And your have " + femaleLifeExpectancy + "more jupiter years to live.");
+      const jupiterFemaleOutput = (`You are ${jupiterAge} in Jupiter. And your have ${femaleLifeExpectancy} more Jupiter years to live.`);
       return jupiterFemaleOutput;
     }
     else if (gender === "male") {
